60d00ffb18addfe241dbf40bf7082d6b53386017
mammadli <gulnar.mammadli@ut.ee>
2023-11-26 02:32:00 +0200

    Create initial setup and folders

---
+++ env.js
@@ -0,0 +1,10 @@
+const envalid = require("envalid");
+const { str, num } = require("envalid");
+
+module.exports = envalid.cleanEnv(process.env, {
+  DB_NAME: str({ default: "library_management_db" }),
+  DB_HOST: str({ default: "localhost" }),
+  DB_PORT: num({ default: 3306 }),
+  DB_USER: str({ default: "root" }),
+  DB_PASSWORD: str({ default: "123456" }),
+});
