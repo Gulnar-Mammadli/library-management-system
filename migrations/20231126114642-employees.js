6779138168c1224ecfc8de8a48f3d1b392bdac72
farida.rustamova <feride.rli@gmail.com>
2023-12-01 18:48:30 +0200

    delete migrations js files

--- migrations/20231126114642-employees.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.renameColumn("employees", "user_id", "userId");
-  }
-};
